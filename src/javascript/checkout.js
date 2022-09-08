aa275a402dc1a495515db095e7d5eab6f5099333
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2022-09-08 16:32:32 +0300

    js checkout api

---
+++ src/javascript/checkout.js
@@ -0,0 +1,30 @@
+var url = "https://sandbox.helapass.com/api/mer_transfer";
+
+var xhr = new XMLHttpRequest();
+xhr.open("POST", url);
+
+xhr.setRequestHeader("Content-Type", "application/json");
+
+xhr.onreadystatechange = function () {
+   if (xhr.readyState === 4) {
+      console.log(xhr.status);
+      console.log(xhr.responseText);
+   }};
+
+var data = `{
+  "merchant_key":"MERCHANT KEY",
+  "public_key":"PUBLIC KEY",
+  "callback_url":"https://www.mydomain.com/success.html",
+  "return_url":"https//www.mydomain.com/return.html",
+  "tx_ref":"REF_usergenerated",
+  "amount":"10000",
+  "email":"mail@example.com",
+  "first_name":"Finn",
+  "last_name":"Marshal",
+  "title":"RPI",
+  "description":"Payment For RPI",
+  "quantity":"10",
+  "currency":"87"
+}`;
+
+xhr.send(data);
